9c3f6f8cd5b8287eb5572778b101d88d06316be0
Faruk1301 <faruk.cse.pust12@gmail.com>
2025-12-31 22:49:37 +0600

    Added CI/CD pipeline

--- provider.tf
+++ provider.tf
@@ -1,10 +1,20 @@
 terraform {
   required_version = ">= 1.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+
+  # S3 Backend with DynamoDB Locking
+  backend "s3" {
+    bucket         = "faruk-terraform-lab-001"
+    key            = "vpc/terraform.tfstate" # Eiti state file-er path
+    region         = "us-east-1"             # Apnar bucket-er region
+    dynamodb_table = "terraform-lock"        # Apnar DynamoDB table name
+    encrypt        = true
   }
 }
 
